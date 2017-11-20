e7f9799ece68ade59be7f63c2c586540828f6cde
Zain ul abidin <zain@aligent.com.au>
2017-11-20 14:26:24 +1030

    util: fix path in util directory

--- util/perms/fix_global_perms.sh
+++ util/perms/fix_global_perms.sh
@@ -2,5 +2,6 @@
 
 #can be run continously like watch .util/fix_global_perms.sh
 CURRENT_FOLDER=$(dirname $(readlink -f "$0"))
-TARGET_FOLDER="$CURRENT_FOLDER/../magento"
-sudo sudo find "$TARGET_FOLDER" ! -user "$USER" -exec sudo chown "$USER": {} +
+TARGET_FOLDER="$CURRENT_FOLDER/../../magento"
+#sudo find "$TARGET_FOLDER" ! -user "$USER" -exec sudo chown "$USER": {} +
+sudo find "$TARGET_FOLDER" ! -user "$USER" ! -path "$TARGET_FOLDER/magento/generated"  -exec sudo chown "$USER": {} +
--- util/perms/fix_root_perm.sh
+++ util/perms/fix_root_perm.sh
@@ -2,7 +2,7 @@
 
 #can be run continously like watch ./util/fix_perm.sh
 CURRENT_FOLDER=$(dirname $(readlink -f "$0"))
-TARGET_FOLDER="$CURRENT_FOLDER/../magento"
+TARGET_FOLDER="$CURRENT_FOLDER/../../magento"
 ls -la "$TARGET_FOLDER" | grep magento
 #need permission of
 sudo chown "$USER":"$USER" "$TARGET_FOLDER/magento"
