d3f948ba93f5d9b68f5cc04449be692ec47e6648
Zain ul abidin <zain@aligent.com.au>
2017-11-25 14:03:48 +1030

    composer: composer folder mounted locally, perms errors friendly

--- util/perms/fix_global_perms.sh
+++ util/perms/fix_global_perms.sh
@@ -3,5 +3,9 @@
 #can be run continously like watch .util/fix_global_perms.sh
 CURRENT_FOLDER=$(dirname $(readlink -f "$0"))
 TARGET_FOLDER="$CURRENT_FOLDER/../../magento"
+#translate ../ into actual path for readibility
+TARGET_FOLDER=$(readlink -m "$TARGET_FOLDER")
 #sudo find "$TARGET_FOLDER" ! -user "$USER" -exec sudo chown "$USER": {} +
 sudo find "$TARGET_FOLDER" ! -user "$USER" ! -path "$TARGET_FOLDER/magento/generated"  -exec sudo chown "$USER": {} +
+echo ""
+echo "ignore some errors for symlinks as they are only valid inside docker container"
