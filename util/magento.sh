bac9209e074d51bac73d05cc9f15cfff40fc328d
Zain ul abidin <zain@aligent.com.au>
2017-12-11 16:55:32 +1030

    util: magento change directory and run bin/magento

--- util/magento.sh
+++ util/magento.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose exec cli magento-command "$@"
+docker-compose exec cli bash -c "cd /var/www/magento && magento-command $@"
+#echo  "cd /var/www/magento && magento-command $@"
