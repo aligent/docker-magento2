0efabb6b251d8442741f97318b4da5976810e1a4
Zain ul abidin <zain@aligent.com.au>
2017-12-22 18:16:38 +1030

    perms: script to fix write permissions

---
+++ util/perms/fix_write_perm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo chmod a+w magento/magento/app/etc/
+sudo chmod a+w magento/magento/pub/static -R
+sudo chmod a+w magento/magento/generate -R
+sudo chmod a+w magento/magento/media -R
+sudo chmod a+w magento/magento/var -R
