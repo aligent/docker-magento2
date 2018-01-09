b78a6725bf6c0de8544f92a0473371b166d6515f
Zain ul abidin <zain@aligent.com.au>
2018-01-09 16:09:07 +1030

    fix permission by changing owner of directories

--- util/perms/fix_global_perms.sh
+++ util/perms/fix_global_perms.sh
@@ -2,10 +2,16 @@
 
 #can be run continously like watch .util/fix_global_perms.sh
 CURRENT_FOLDER=$(dirname $(readlink -f "$0"))
-TARGET_FOLDER="$CURRENT_FOLDER/../../magento"
+TARGET_FOLDER="$CURRENT_FOLDER/../../magento/magento"
 #translate ../ into actual path for readibility
 TARGET_FOLDER=$(readlink -m "$TARGET_FOLDER")
-#sudo find "$TARGET_FOLDER" ! -user "$USER" -exec sudo chown "$USER": {} +
-sudo find "$TARGET_FOLDER" ! -user "$USER" ! -path "$TARGET_FOLDER/magento/generated"  -exec sudo chown "$USER": {} +
-echo ""
-echo "ignore some errors for symlinks as they are only valid inside docker container"
+#echo "target folder is $TARGET_FOLDER"
+MATCHES=$(sudo find "$TARGET_FOLDER"  ! -user "$USER"  ! -path "$TARGET_FOLDER/vendor*" ! -path "$TARGET_FOLDER/update*" ! -path "$TARGET_FOLDER/pub/static*" ! -path "$TARGET_FOLDER/var*" -print | wc -l)
+echo "MATCHES $MATCHES"
+if [[ "$MATCHES" == "0" ]]
+then
+    echo "Nothing to chown"
+else
+   echo "changing in the batches of 1000"
+   sudo find "$TARGET_FOLDER"  ! -user "$USER"  ! -path "$TARGET_FOLDER/vendor*" ! -path "$TARGET_FOLDER/update*" ! -path "$TARGET_FOLDER/pub/static*" ! -path "$TARGET_FOLDER/var*" -print | head -1000 | xargs  sudo chown "$USER"
+fi
