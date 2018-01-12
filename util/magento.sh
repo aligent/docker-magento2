9d19ce0831b24670b85141f1a750433ea4cd5289
Zain ul abidin <zain@aligent.com.au>
2018-01-12 10:39:02 +1030

    util: params to docker bash commands

--- util/magento.sh
+++ util/magento.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose exec cli bash -c "cd /var/www/magento && magento-command $@"
-#echo  "cd /var/www/magento && magento-command $@"
+command_string="$@"
+docker-compose exec cli bash -c -- "cd /var/www/magento && magento-command $command_string"
--- util/n98-magerun2.sh
+++ util/n98-magerun2.sh
@@ -2,4 +2,4 @@
 
 COMMAND="cd /var/www/magento && n98-magerun2.phar $@";
 #echo "$COMMAND";
-docker-compose exec cli bash -c "$COMMAND"
+docker-compose exec cli sudo -u www-data bash -c "$COMMAND"
