3636a07281d3314dd9275da5ab1bc210b7ea4f82
Yury Ostapenko <yury.ostapenko@aligent.com.au>
2018-01-18 12:09:24 +1030

    Util Fix Write Permissions: Fixed paths

--- util/perms/fix_write_perm.sh
+++ util/perms/fix_write_perm.sh
@@ -2,6 +2,6 @@
 
 sudo chmod a+w magento/magento/app/etc/
 sudo chmod a+w magento/magento/pub/static -R
-sudo chmod a+w magento/magento/generate -R
-sudo chmod a+w magento/magento/media -R
+sudo chmod a+w magento/magento/generated -R
+sudo chmod a+w magento/magento/pub/media -R
 sudo chmod a+w magento/magento/var -R
