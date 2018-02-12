448a1be2331b893b600b8db03b68170e12073e49
Zain ul abidin <zain@aligent.com.au>
2018-02-12 12:42:49 +1030

    composer: continue to run it as root to avoid composer cache issues

--- util/composer.sh
+++ util/composer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose exec cli sudo -u www-data bash -c "cd /var/www/magento && composer $@"
+docker-compose exec cli sudo bash -c "cd /var/www/magento && composer $@"
