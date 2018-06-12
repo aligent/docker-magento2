b55d7de670796d4f328fdd4d434b61cddbb20da4
Zain ul abidin <zain@aligent.com.au>
2018-06-12 13:56:19 +0930

    xdebug_cli: fixed typo in hostname detection

--- util/xdebug.sh
+++ util/xdebug.sh
@@ -8,8 +8,7 @@ fi
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 script_path="$parent_path/get_default_host.sh"
-hostname="($script_path)"
-
+hostname="$($script_path)"
 COMMAND="cd /var/www/magento && export PHP_IDE_CONFIG=\"serverName=$hostname\" && php -dxdebug.remote_enable=1 -dxdebug.remote_autostart=1 -dxdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3}'`  $@";
 #echo "$COMMAND";
 docker-compose exec cli sudo -u www-data bash -c "$COMMAND"
